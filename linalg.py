afec4492095be72889cc776fb4f9730fbf9553a2
Jamie Lawson <jlawson@uoguelph.ca>
2015-10-15 21:11:34 -0400

    Replace length property with a function. For numpy compatibilty.

--- linalg.py
+++ linalg.py
@@ -217,9 +217,7 @@ class matrix(object):
         """
         return [self.m*self.n, self.m, self.n][axis]
 
-    # to be replaced by def len(self)
-    @property
-    def length(self):
+    def __len__(self):
         return self.m
 
     @property
@@ -272,19 +270,19 @@ def det_inv(x):
     # divide each row element by [0] to give a one in the first position
     # (may have to find a row to switch with if first element is 0)
     x = x.copy()
-    inverse = eye(x.length)
+    inverse = eye(len(x))
     sign = 1
     factors = []
     p = 0
-    while p < x.length:
+    while p < len(x):
         d = x[p, p]
         if abs(d) < 1e-30:
             # pivot == 0 need to swap a row
             # need to check if swap row also has a zero at the same position
             np = 1
-            while (p+np) < x.length and x[p+np, p] < 1e-30:
+            while (p+np) < len(x) and x[p+np, p] < 1e-30:
                 np += 1
-            if (p+np) == x.length:
+            if (p+np) == len(x):
                 # singular
                 return [0, []]
             # swap rows
@@ -300,32 +298,32 @@ def det_inv(x):
             continue
         factors.append(d)
         # change target row
-        for n in range(p,x.length):
+        for n in range(p,len(x)):
             x[p,n] = x[p,n] / d
         # need to do the entire row for the inverse
-        for n in range(x.length):
+        for n in range(len(x)):
             inverse[p,n] = inverse[p,n] / d
         # eliminate position in the following rows
-        for i in range(p+1,x.length):
+        for i in range(p+1,len(x)):
             # multiplier is that column entry
             t = x[i,p]
-            for j in range(p,x.length):
+            for j in range(p,len(x)):
                 x[i,j] = x[i,j] - (t * x[p,j])
-            for j in range(x.length):
+            for j in range(len(x)):
                 inverse[i,j] = inverse[i,j] - (t * inverse[p,j])
         p += 1
     s = sign
     for i in factors:
         s = s * i # determinant
     # travel through the rows eliminating upper diagonal non-zero values
-    for i in range(x.length-1):
+    for i in range(len(x)-1):
         # final row should already be all zeros except for the final position
-        for p in range(i+1,x.length):
+        for p in range(i+1,len(x)):
             # multiplier is that column entry
             t = x[i,p]
-            for j in range(i+1,x.length):
+            for j in range(i+1,len(x)):
                 x[i,j] = x[i,j] - (t * x[p,j])
-            for j in range(x.length):
+            for j in range(len(x)):
                 inverse[i,j] = inverse[i,j] - (t * inverse[p,j])
     return (s, inverse)
 
